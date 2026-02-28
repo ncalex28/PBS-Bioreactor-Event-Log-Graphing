bcaebb4a173e6ae10b68ccf83b2de52f0d8491d8
ncalex28 <ncalex28@gmail.com>
2026-02-28 10:20:52 -0500

    Update bioreactor_extraction.py

--- bioreactor_extraction.py
+++ bioreactor_extraction.py
@@ -2,16 +2,17 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import io
 
 st.title("Bioreactor Data Viewer")
 
-# Upload CSV
 uploaded_file = st.file_uploader("Upload your reactor CSV", type="csv")
+
 if uploaded_file is not None:
-    
+
     df = pd.read_csv(uploaded_file)
-    
-    # ---- Tidying automatically ----
+
+    # ---- Tidy automatically ----
     tidy_list = []
     for col in df.columns:
         if col.endswith(".1"):
@@ -23,21 +24,24 @@ if uploaded_file is not None:
             temp["variable"] = col
             temp["time"] = pd.to_datetime(temp["time"])
             tidy_list.append(temp)
-    
+
     tidy = pd.concat(tidy_list, ignore_index=True).sort_values("time")
-    
+
     st.success("Data tidied successfully!")
-    
-    # ---- Select variables to plot ----
+
+    # ---- Variable selection ----
     all_vars = tidy["variable"].unique().tolist()
+
     selected_vars = st.multiselect(
-        "Select variables to plot", all_vars, default=all_vars[:5]
+        "Select variables to plot",
+        all_vars,
+        default=all_vars[:5]
     )
-    
+
     if selected_vars:
+
         plot_df = tidy.query("variable in @selected_vars")
-        
-        # ---- Faceted line plot ----
+
         fig = px.line(
             plot_df,
             x="time",
@@ -45,8 +49,20 @@ if uploaded_file is not None:
             facet_row="variable",
             height=300 * len(selected_vars)
         )
+
         fig.update_yaxes(matches=None)
         fig.update_xaxes(matches="x")
         fig.update_layout(showlegend=False)
-        
+
         st.plotly_chart(fig, use_container_width=True)
+
+        # ---- HTML export ----
+        html_buffer = io.StringIO()
+        fig.write_html(html_buffer, full_html=True)
+
+        st.download_button(
+            label="Download Interactive HTML",
+            data=html_buffer.getvalue(),
+            file_name="bioreactor_plot.html",
+            mime="text/html"
+        )
